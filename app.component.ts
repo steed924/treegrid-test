44d2e960bcde4b5ca5d006f5a1217de9b5738e15
Steed924 <steed924@outlook.com>
2021-10-29 04:26:04 +0900

    2 files modified

--- app.component.ts
+++ app.component.ts
@@ -1,20 +1,24 @@
-import { Component , OnInit , ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 import { TreeGridComponent } from '@syncfusion/ej2-angular-treegrid';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: 'app.component.html'
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
-    public data: DataManager;
-    public pageSetting: Object;
+  public data: DataManager;
+  public pageSetting: Object;
 
-    @ViewChild('treegrid')
-    public treegrid: TreeGridComponent;
+  @ViewChild('treegrid')
+  public treegrid: TreeGridComponent;
 
-    ngOnInit(): void {
-        this.data =  new DataManager({ url: 'https://ej2services.syncfusion.com/production/web-services/api/SelfReferenceData', adaptor: new WebApiAdaptor });
-        this.pageSetting = { pageCount: 3 };
-    }
+  ngOnInit(): void {
+    this.data = new DataManager({
+      url: 'https://ej2services.syncfusion.com/production/web-services/api/SelfReferenceData',
+      adaptor: new WebApiAdaptor(),
+      offline: true,
+    });
+    this.pageSetting = { pageCount: 3 };
+  }
 }
