0596ca32e8615fcd57fa25832197bdb971cc34e1
Steed924 <steed924@outlook.com>
2021-10-29 03:58:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# treegrid-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/treegrid-test)
---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component , OnInit , ViewChild} from '@angular/core';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+import { TreeGridComponent } from '@syncfusion/ej2-angular-treegrid';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
+})
+export class AppComponent {
+    public data: DataManager;
+    public pageSetting: Object;
+
+    @ViewChild('treegrid')
+    public treegrid: TreeGridComponent;
+
+    ngOnInit(): void {
+        this.data =  new DataManager({ url: 'https://ej2services.syncfusion.com/production/web-services/api/SelfReferenceData', adaptor: new WebApiAdaptor });
+        this.pageSetting = { pageCount: 3 };
+    }
+}
